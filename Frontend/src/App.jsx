597060a12b2f6aff7da81f03545e4cf8885f5dfc
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-02-03 07:36:56 +0530

    updated Techstack.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import Layout from "./components/Layouts/Layout.jsx";
 import About from "./pages/About/About.jsx";
+import Techstack from "./pages/TechStack/Techstack.jsx";
 
 export default function App(){
   return(
@@ -7,7 +8,7 @@ export default function App(){
       <Layout/>
       <div className="container">
       <About/>
-
+      <Techstack/>
       </div>
     </>
   )
---
+++ Frontend/src/pages/TechStack/Techstack.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Techstack.css';
+
+const Techstack = () => {
+  return (
+    <>
+        <div className="container techstack">
+            <h2>Technologies Stack</h2>
+            <p>👉 including programming languages,frameworks, database, frontend, backend, and API's</p>
+        </div>
+    </>
+  )
+}
+
+export default Techstack
