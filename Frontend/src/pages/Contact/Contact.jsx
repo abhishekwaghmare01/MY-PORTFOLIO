fdf41796e70ec1406564f8c146804620792ad4da
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-02-06 21:30:55 +0530

    updated contact.jsx

--- Frontend/src/pages/Contact/Contact.jsx
+++ Frontend/src/pages/Contact/Contact.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React from "react";
 import './Contact.css';
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
+
 
 const Contact = () => {
   return (
@@ -7,19 +10,69 @@ const Contact = () => {
       <div className="container contact">
         <div className="card card0 border-0">
           <div className="row">
-              <div className="col-md-6 con-lg-6 col-xk-6 col-sm-12 ">
+            <div className="col-md-6 con-lg-6 col-xl-6 col-sm-12 ">
+              <div className="card1">
+                <div className="row border-line">
+                  <img
+                    src="https://st3.depositphotos.com/1385248/14794/i/450/depositphotos_147947683-stock-photo-hands-with-contact-us-words.jpg"
+                    alt="contact"
+                    className="image"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-6">
+              <div className="card2 d-flex card border-0 px-4 py-3">
+                <div className="row">
+                  <div className="row">
+                    <h4>Contact with
+                    <FaLinkedin color="#2e619b" size={30} className="ms-2"/>
+                    <FaGithubSquare  size={30} className="ms-2"/>
+                    </h4>
+                  </div>
 
-                <div className="card1">
-                  <div className="row border-line">
-                      <img src="https://st3.depositphotos.com/1385248/14794/i/450/depositphotos_147947683-stock-photo-hands-with-contact-us-words.jpg" alt="contact" />
+                  <div className="row px-3 mb-4">
+                    <div className="line" />
+                    <small className="or text-center">OR</small>
+                    <div className="line" />
+                  </div>
+                  <div className="row px-3 ">
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder="Enter your Good Name"
+                      className="mb-3"
+                    />
+                  </div>
+                  <div className="row px-3">
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Enter your E-mail Address"
+                      className="mb-3"
+                    />
+                  </div>
+                  <div className="row px-3">
+                    <textarea
+                      type="text"
+                      name="msg"
+                      placeholder="Write your message"
+                      className="mb-3"
+                    />
+                  </div>
+                  <div className="row px-3">
+                    <button className="button" type="submit">
+                      SEND MESSAGE
+                    </button>
                   </div>
                 </div>
               </div>
+            </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
