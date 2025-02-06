eac98248700a5b9a918c6f6c2b252e00b4af34ab
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-02-06 19:40:15 +0530

    Updated Contact.jsx
    add new classes

--- Frontend/src/pages/Contact/Contact.jsx
+++ Frontend/src/pages/Contact/Contact.jsx
@@ -4,7 +4,20 @@ import './Contact.css';
 const Contact = () => {
   return (
     <>
-      
+      <div className="container contact">
+        <div className="card card0 border-0">
+          <div className="row">
+              <div className="col-md-6 con-lg-6 col-xk-6 col-sm-12 ">
+
+                <div className="card1">
+                  <div className="row border-line">
+                      <img src="https://st3.depositphotos.com/1385248/14794/i/450/depositphotos_147947683-stock-photo-hands-with-contact-us-words.jpg" alt="contact" />
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
