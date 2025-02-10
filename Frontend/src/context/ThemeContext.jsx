399e010c419ca4f54c30b65a91a489f020356d19
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-02-10 20:48:59 +0530

    updated Home.jsx

---
+++ Frontend/src/context/ThemeContext.jsx
@@ -0,0 +1,23 @@
+// global state ko manage krte hai---->creatre state --> koi bhi component ko directly access kr sakte haii nahi to hame as a prop hame pass krana padta haii isko avoid krne ke liye hum global state banate hai.
+
+//coustom hook => we create a coustom hook for the use of functionality with the help of context api.
+
+import { useState, createContext, useContext } from "react";
+//to create context
+const ThemeContext = createContext();
+//if wo want to use context so we have to create a provider
+// to create provider
+
+const ThemeProvider = ({ children }) => {
+  const [theme, useTheme] = useState("light");
+  return (
+    <ThemeContext.Provider value={[theme, useTheme]}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+//CUSTOM HOOK
+const useTheme = () => useContext(ThemeContext);
+
+export { useTheme, ThemeProvider };
--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,28 +1,47 @@
 import React from "react";
+import { useTheme } from "../../context/ThemeContext";
 import "./Home.css";
 import Typewriter from "typewriter-effect";
-import abhiCV from '../../assets/docs/abhiCV.pdf';
+import abhiCV from "../../assets/docs/abhiCV.pdf";
+import { BsMoonStarsFill } from "react-icons/bs";
+import { GiSunflower } from "react-icons/gi";
 
-const Home = () => (
-  <>
-    <div className="container-fluid home-container " id="home">
-      <div className="container home-content">
-        <h2>𝐇𝐢 👋 𝐈'𝐦 𝐚</h2>
-        <h1>
-          <Typewriter
-            options={{
-              strings: ["Full Stack Web Developer !", "Full Stack JAVA Developer !", "MERN Stack Developer !", "MEAN Stack Developer !"],
-              autoStart: true,
-              loop: true,
-            }} />
-        </h1>
-        <div className="home-buttons">
-          <button className="btn btn-hire">Hire Me!</button>
-          <a className="btn btn-cv " href={abhiCV} download="AbhishekCV.pdf">My Resume</a>
+const Home = () => {
+  const [theme, setTheme] = useTheme();
+  return (
+    <>
+      <div className="container-fluid home-container " id="home">
+        <div className="theme-btn">
+          {
+            theme === 'light' ? (<BsMoonStarsFill/>):(< >)
+          }
+        </div>
+        <div className="container home-content">
+          <h2>𝐇𝐢 👋 𝐈'𝐦 𝐚</h2>
+          <h1>
+            <Typewriter
+              options={{
+                strings: [
+                  "Full Stack Web Developer !",
+                  "Full Stack JAVA Developer !",
+                  "MERN Stack Developer !",
+                  "MEAN Stack Developer !",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h1>
+          <div className="home-buttons">
+            <button className="btn btn-hire">Hire Me!</button>
+            <a className="btn btn-cv " href={abhiCV} download="AbhishekCV.pdf">
+              My Resume
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Home;
