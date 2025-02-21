58b5085083dd4ec86a704fb33b4fcb703ad1b828
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-02-21 20:12:18 +0530

    Update About.jsx

--- Frontend/src/pages/About/About.jsx
+++ Frontend/src/pages/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./About.css";
-//import Img from "./Images/Profile_photo.jpg";
+
 import Img from "/src/Images/Profile_photo.jpg";
 
 const About = () => {
@@ -23,14 +23,7 @@ const About = () => {
           <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-content">
             <h1>About me</h1>
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
-              dolores sint consequuntur, possimus velit neque aut, facere
-              mollitia reprehenderit architecto error qui eaque illum. Eveniet
-              labore voluptatem quia dolorum accusamus. Accusantium aliquam id,
-              temporibus corporis blanditiis velit provident similique
-              exercitationem itaque repudiandae facere impedit maiores animi
-              suscipit sapiente praesentium nobis reiciendis aperiam amet.
-              Velit, numquam eligendi earum deserunt illo esse.
+            I am Abhishek Waghmare, a results-driven software developer with hands-on experience in full-stack web development and the MERN stack. Skilled in designing scalable applications, implementing secure APIs, and optimizing performance, I have a strong foundation in Java, JavaScript, React.js, Node.js, SQL, NoSQL, and Spring Boot. I have completed multiple projects, including a real-time video conferencing platform, a job portal, and a certificate verification system. With internship experience at Zidio Development and YHills Edutech, I am passionate about solving complex problems, collaborating with teams, and continuously learning new technologies.
             </p>
           </div>
         </div>
