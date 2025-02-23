bf5e00a5438adf833a71ef318ab8ffb931de5d99
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-02-23 21:40:15 +0530

    Update Layout.jsx

--- Frontend/src/components/Layouts/Layout.jsx
+++ Frontend/src/components/Layouts/Layout.jsx
@@ -1,39 +1,39 @@
 //rafce - shortcut
-import React from 'react';
-import { useState } from 'react';
-import Home from '../../pages/Home/Home';
+import React from "react";
+import { useState } from "react";
+import Home from "../../pages/Home/Home";
 import { AiOutlineDoubleLeft, AiOutlineDoubleRight } from "react-icons/ai";
-import './Layout.css';
-import Menus from '../Menus/Menus';
-
+import "./Layout.css";
+import Menus from "../Menus/Menus";
 
 const Layout = () => {
-const [toggle, setToggle] = useState(true);
-const handelToggle = () =>{
-     setToggle(!toggle);
-}
+  const [toggle, setToggle] = useState(true);
+  const handelToggle = () => {
+    setToggle(!toggle);
+  };
 
   return (
-   <>
-    <div className="sidebar-section">
-        <div className= {toggle ? "sidebar-toggle sidebar" : "sidebar"}>
-            <div className="sidebar-toggle-icons">
-               
-                <p onClick={handelToggle}>
-                    {
-                        toggle ? (<AiOutlineDoubleLeft size={30} />) : (<AiOutlineDoubleRight size={30} />)
-                    }
-                </p> 
-            </div>  
-             {/* manupulate as a props  */}
-             <Menus toggle={toggle}/>  
+    <>
+      <div className="sidebar-section ">
+        <div className={toggle ? "sidebar-toggle sidebar" : "sidebar"}>
+          <div className="sidebar-toggle-icons">
+            <p onClick={handelToggle}>
+              {toggle ? (
+                <AiOutlineDoubleLeft size={30} />
+              ) : (
+                <AiOutlineDoubleRight size={30} />
+              )}
+            </p>
+          </div>
+          {/* manupulate as a props  */}
+          <Menus toggle={toggle} />
         </div>
         <div className="container">
-        <Home/>
+          <Home />
         </div>
-    </div>
-   </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
