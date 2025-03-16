e4cd4e9137184be7269ad9b59a9b896767c0e3d0
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-03-16 19:24:59 +0530

    Update Project.jsx

--- Frontend/src/pages/Projects/Project.jsx
+++ Frontend/src/pages/Projects/Project.jsx
@@ -111,6 +111,60 @@ const Project = () => {
               </div>
             </div>
           </div>
+          <div className="col-md-4" style={{ paddingTop: "28px" }}>
+            <div className="card rounded">
+              <div className="card-image">
+                <span className="card-notify-badge">Full Stack Project</span>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SuuNrgIeWk_TlxfGyGG85zFTMes2cKxHsg&s"
+                  alt="project1"
+                />
+              </div>
+              <div className="card-image-overly m-auto mt-3">
+                <span className="card-detail-badge">NodeJS</span>
+                <span className="card-detail-badge">ReactJs</span>
+                <span className="card-detail-badge">ExpressJS</span>
+                <span className="card-detail-badge">MongoDB</span>
+              </div>
+              <div className="card-body text-center ">
+                <div className="ad-title m-auto">
+                  <h5 className="text-uppercase ">
+                    Certificate management system
+                  </h5>
+                </div>
+                <a href="#" className="ad-btn">
+                  View
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4" style={{ paddingTop: "28px" }}>
+            <div className="card rounded">
+              <div className="card-image">
+                <span className="card-notify-badge">Full Stack Project</span>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9SuuNrgIeWk_TlxfGyGG85zFTMes2cKxHsg&s"
+                  alt="project1"
+                />
+              </div>
+              <div className="card-image-overly m-auto mt-3">
+                <span className="card-detail-badge">NodeJS</span>
+                <span className="card-detail-badge">ReactJs</span>
+                <span className="card-detail-badge">ExpressJS</span>
+                <span className="card-detail-badge">MongoDB</span>
+              </div>
+              <div className="card-body text-center ">
+                <div className="ad-title m-auto">
+                  <h5 className="text-uppercase ">
+                    Certificate management system
+                  </h5>
+                </div>
+                <a href="#" className="ad-btn">
+                  View
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
